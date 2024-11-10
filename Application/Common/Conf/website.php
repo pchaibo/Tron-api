e4751e4f5fb4cf8033007f16a1e6a569c256c035
pchaibo <pchaibo@163.com>
2024-11-10 11:04:24 +0800

    go

--- Application/Common/Conf/website.php
+++ Application/Common/Conf/website.php
@@ -1,6 +1,6 @@
 <?php 
 		return array(
-			'WEB_TITLE' => '恒腾云支付',
+			'WEB_TITLE' => 'TRC20 USDT 支付系统',
 			'DOMAIN' => '127.0.0.189',
 			'MODULE_ALLOW_LIST'   => array('Home','User','Manage','Install', 'Weixin','Pay','Cashier'),
 			'URL_MODULE_MAP'  => array('manage'=>'admin'),
